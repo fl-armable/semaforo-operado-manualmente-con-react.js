2172431900d6a200961c4a18e28216c1570beaa5
fl-armable <fl.irlanda@gmail.com>
2025-05-06 08:07:11 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,31 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const TrafficLight = () => {
+	const [color,setColor] = useState("green");
+	const [otherColor,setOtherColor] = useState(false);
+	
+	const alternateColor = () => {
+		let listColor = ['green', 'yellow', 'red'];
+		if(otherColor){
+			listColor.push('purple');
+		}
+		let index = listColor.indexOf(color);
+		setColor (listColor[(index + 1) % listColor.length]); // Vuelve a 0 cuando index es -1 o es length-1
+	}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+	<div>
+		<button onClick={alternateColor} className="button">Alternate color</button>
+		<div className="box">
+			<button onClick={() => setColor('red')} className={`red light ${color === "red" ? "active" : ""}`}></button>
+			<button onClick={() => setColor('yellow')} className={`yellow light ${color === "yellow" ? "active" : ""}`}></button>
+			<button onClick={() => setColor('green')} className={`green light ${color === "green" ? "active" : ""}`}></button>
+			{ otherColor? <button onClick={() => setColor('purple')} className={`purple light ${color === "purple" ? "active" : ""}`}></button> : null }
 		</div>
+		<button onClick={() => setOtherColor(!otherColor)} className="button">{otherColor ? "Remove purple" : "Add purple"}</button>
+	</div>
 	);
 };
 
-export default Home;
+export default TrafficLight;
